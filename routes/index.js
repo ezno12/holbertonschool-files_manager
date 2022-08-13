0d6d9884d2e62ea0865c8cb111c12db83110e910
tennin12 <bassembenyhia12@gmail.com>
2022-08-13 14:33:48 +0100

    export router added and style fixed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 import AppController from '../controllers/AppController';
 
-
 const express = require('express');
 // all endpoints of our API
 const router = (app) => {
@@ -10,4 +9,6 @@ const router = (app) => {
 
   route.get('/status', (request, response) => AppController.getStatus(request, response));
   route.get('/stats', (request, response) => AppController.getStats(request, response));
-}
+};
+
+export default router;
